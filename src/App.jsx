969d995137f308bf7ed65e9c7490c96b5bfe9359
Olya <o.us@sw-tech.by>
2024-01-25 13:53:50 +0300

    Create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { Layout } from 'antd';
+import AppHeader from './components/layout/AppHeader';
+import AppSlider from './components/layout/AppSlider';
+import AppContent from './components/layout/AppContent';
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+  return (
+    <Layout>
+      <AppHeader/>
+      <Layout>
+        <AppSlider/>
+        <AppContent/>
+      </Layout>
+    </Layout>
+  )
 }
